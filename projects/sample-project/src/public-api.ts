4924dda074f2407812858444804d2d3465b90d06
Pandurang Mote <pandurangm@versionsolutions.com>
2022-01-07 20:09:18 +0530

    code corrections

---
+++ projects/sample-project/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of sample-project
+ */
+
+// export * from './lib/sample-project.service';
+export * from './lib/sample-project.component';
+export * from './lib/sample-project.module';
